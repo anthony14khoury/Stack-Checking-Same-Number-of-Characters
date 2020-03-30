ab61e21fd4e28aae78a3a756b6a2a596e3056ff9
anthony14khoury <anthony14khoury@gmail.com>
2020-03-30 14:01:52 -0400

    Updated File

--- main.cpp
+++ main.cpp
@@ -2,43 +2,60 @@
 #include <stack>
 using namespace std;
 
-bool inLanguage(char* theString)
+bool inLanguage(string theString)
 {
-     stack<char> theStack;
-     int i = 0;
+    stack<char> theStack;
+    int i = 0;
 
-     while(theString[i] != NULL)
-     {
-          if ((theString[i] != 'A') && (theString[i] != 'B'))
-          {
-               return false;
-          }
+    while (theString[i] != NULL)
+    {
+        if ((theString[i] != 'A') && (theString[i] != 'B'))
+        {
+            return false;
+        }
 
-          if(theStack.empty())
-          {
-               theStack.push(theString[i]);
-          }
-          else
-          {
-               if (theStack.top() == theString[i])
-               {
-                    theStack.push(theString[i]);
-               }
-               else
-               {
-                    theStack.pop();
-               }
-          }
+        if (theStack.empty())
+        {
+            theStack.push(theString[i]);
+        }
+        else
+        {
+            if (theStack.top() == theString[i])
+            {
+                theStack.push(theString[i]);
+            }
+            else
+            {
+                theStack.pop();
+            }
+        }
 
-          i++;
-     }
+        i++;
+    }
 
-     if(theStack.empty())
-     {
-          return true;
-     }
-     else
-     {
-          return false;
-     }
+    if (theStack.empty())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+int main()
+{
+    string temp = "";
+    cin >> temp;
+    if (inLanguage(temp) == false)
+    {
+        cout << "False" << endl;
+        return false;
+    }
+    else
+    {
+        cout << "True" << endl;
+        return true;
+    }
 }
